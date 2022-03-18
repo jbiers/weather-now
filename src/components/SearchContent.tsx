2cc417e0bfd7ed1f3c5edefe52ddab60f59833c8
jbiers <juliabiersuriano@gmail.com>
2022-03-18 09:40:11 -0400

    added search

---
+++ src/components/SearchContent.tsx
@@ -0,0 +1,14 @@
+export function SearchContent() {
+    return (
+        <div className="searchContent">
+            <input
+                className="searchCity"
+                type="text"
+                placeholder="Search city">
+            </input>
+            <button className="btn">search</button>
+        </div>
+    )
+}
+
+/** magnifying glass instead of search */
--- src/components/WeatherContent.tsx
+++ src/components/WeatherContent.tsx
@@ -1,9 +1,11 @@
+import { SearchContent } from './SearchContent'
+
 import '../styles/weatherContent.scss'
 
 export function WeatherContent() {
     return (
         <section className="weatherContent">
-
+            <SearchContent />
         </section>
     )
 }
